d3235ce6a503f099c974bb21d506573afb88267f
Otavio Palma <otaviopalma@gmail.com>
2021-03-01 11:02:28 -0300

    feat: users-appointments relationship

--- src/database/migrations/1614602063976-CreateAppointments.ts
+++ src/database/migrations/1614602063976-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1614602063976
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
