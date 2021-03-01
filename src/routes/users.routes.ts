66941e02a8ba0f46cf2d530e216e7d24a8279400
Otavio Palma <otaviopalma@gmail.com>
2021-03-01 12:52:48 -0300

    feat: global error handling

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -9,15 +9,36 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
+
+  return response.json({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    avatar: user.avatar,
+    created_at: user.created_at,
+    updated_at: user.updated_at,
+  });
+});
+
+usersRouter.patch(
+  '/avatar',
+  ensureSession,
+  upload.single('avatar'),
+  async (request, response) => {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
     });
 
     return response.json({
@@ -28,35 +49,6 @@ usersRouter.post('/', async (request, response) => {
       created_at: user.created_at,
       updated_at: user.updated_at,
     });
-  } catch (err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
-});
-
-usersRouter.patch(
-  '/avatar',
-  ensureSession,
-  upload.single('avatar'),
-  async (request, response) => {
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
-
-      const user = await updateUserAvatar.execute({
-        user_id: request.user.id,
-        avatarFilename: request.file.filename,
-      });
-
-      return response.json({
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        avatar: user.avatar,
-        created_at: user.created_at,
-        updated_at: user.updated_at,
-      });
-    } catch (err) {
-      return response.status(err.statusCode).json({ error: err.message });
-    }
   },
 );
 
