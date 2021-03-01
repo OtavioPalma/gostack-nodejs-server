223497260ec615921ff67650464eb96c6b4dbf3b
Otavio Palma <otaviopalma@gmail.com>
2021-03-01 12:36:05 -0300

    feat: user avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 import ensureSession from '../middlewares/ensureSession';
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -23,6 +24,7 @@ usersRouter.post('/', async (request, response) => {
       id: user.id,
       name: user.name,
       email: user.email,
+      avatar: user.avatar,
       created_at: user.created_at,
       updated_at: user.updated_at,
     });
@@ -37,7 +39,21 @@ usersRouter.patch(
   upload.single('avatar'),
   async (request, response) => {
     try {
-      return response.json({});
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      return response.json({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+      });
     } catch (err) {
       return response.status(400).json({ error: err.message });
     }
