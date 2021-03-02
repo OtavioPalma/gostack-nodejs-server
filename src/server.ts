f331e19e44465b5a150754e67c24906b0318b3fc
Otavio Palma <otaviopalma@gmail.com>
2021-03-02 13:17:41 -0300

    feat: cors

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import 'reflect-metadata';
@@ -7,7 +8,7 @@ import AppError from './errors/AppError';
 import routes from './routes';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
