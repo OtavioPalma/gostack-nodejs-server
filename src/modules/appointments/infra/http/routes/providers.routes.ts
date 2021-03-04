64571318718d9aa238cbf068abd62e4a20b27e61
Otavio Palma <otaviopalma@gmail.com>
2021-03-04 20:08:43 -0300

    feat: availability routes and controller

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,11 +1,24 @@
 import ensureSession from '@modules/users/infra/http/middlewares/ensureSession';
 import { Router } from 'express';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
 import ProvidersController from '../controllers/ProvidersController';
 
 const providersRouter = Router();
+
 const providersController = new ProvidersController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
 
 providersRouter.use(ensureSession);
 providersRouter.get('/', providersController.index);
+providersRouter.post(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index,
+);
+providersRouter.post(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.index,
+);
 
 export default providersRouter;
